2d5d55bb6a13485ef4997bd0f110751960ec6e24
RDRobinson <drobinson@taptoongames.com>
2014-04-28 14:42:43 -0400

    Testing project
    Project commit

---
+++ Assets/Testing.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Testing : MonoBehaviour {
+    public AnimationClip clip;
+
+	// Use this for initialization
+	void Start () {
+        animation.clip = clip;
+        animation.Play();
+	}
+}
